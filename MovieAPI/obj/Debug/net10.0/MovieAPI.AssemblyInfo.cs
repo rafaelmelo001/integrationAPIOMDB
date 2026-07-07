061fe6c7ddeea7d553c060b49a0f821e025e66c8
Rafael Melo <rafahdiaz18@gmail.com>
2026-07-07 18:48:10 -0300

    Criação de Dockerfile, aprendizado com containers, imagense aprendizado com API rodando no container 8080. Consolidação de varios entedimentos. Posteriormente vou publicar a API no render e utilizar o sistema completamente na WEB. Teste com SUCESSO localmente e no container. Falta alguns ajustes

--- MovieAPI/obj/Debug/net10.0/MovieAPI.AssemblyInfo.cs
+++ MovieAPI/obj/Debug/net10.0/MovieAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f9e38de39d8bd112d27f25591344574732b36c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd521c56e9baa1f343f49fb779b060ff830d7eba")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
