fe55ca295bb86a68c5e349a44863d5cd33086a08
Rafael Melo <rafahdiaz18@gmail.com>
2026-07-08 15:24:40 -0300

    Update in the style and visualization for users

--- projetoFilmes/JS/controller/filmController.js
+++ projetoFilmes/JS/controller/filmController.js
@@ -16,27 +16,20 @@ document.getElementById("getFilm").addEventListener("click", async function (){
 
         const dados = await getFilm(nomeFilme);
 
-        display.innerHTML = 
-        `
-        <ul>
-            <li><strong>Título:</strong> ${dados.Title}</li>
-            <li><strong>Ano:</strong> ${dados.Year}</li>
-            <li><strong>Classificação:</strong> ${dados.Rated}</li>
-            <li><strong>Lançamento:</strong> ${dados.Released}</li>
-            <li><strong>Duração:</strong> ${dados.Runtime}</li>
-            <li><strong>Gênero:</strong> ${dados.Genre}</li>
-            <li><strong>Diretor:</strong> ${dados.Director}</li>
-            <li><strong>Roteiristas:</strong> ${dados.Writer}</li>
-            <li><strong>Atores:</strong> ${dados.Actors}</li>
-            <li><strong>Sinopse:</strong> ${dados.Plot}</li>
-            <li><strong>Idioma:</strong> ${dados.Language}</li>
-            <li><strong>País:</strong> ${dados.Country}</li>
-            <li><strong>Prêmios:</strong> ${dados.Awards}</li>
-            <li><strong>IMDb:</strong> ${dados.imdbRating}</li>
-            <li><strong>Votos:</strong> ${dados.imdbVotes}</li>
-            <li><strong>Bilheteria:</strong> ${dados.BoxOffice}</li>
-        </ul>
-        `;
+        display.innerHTML = `
+    <img src="${dados.Poster}" alt="${dados.Title}">
+
+    <div class="movie-info">
+        <h2>${dados.Title}</h2>
+
+        <p><strong>Ano:</strong> ${dados.Year}</p>
+        <p><strong>Diretor:</strong> ${dados.Director}</p>
+        <p><strong>Gênero:</strong> ${dados.Genre}</p>
+        <p><strong>Duração:</strong> ${dados.Runtime}</p>
+        <p><strong>IMDb:</strong> ⭐ ${dados.imdbRating}</p>
+        <p>${dados.Plot}</p>
+    </div>
+`;
 
     }
     catch(error)
